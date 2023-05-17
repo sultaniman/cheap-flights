60f75a0f7bd3fa69f86304cc76a554749cf91387
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-05-17 15:18:11 +0200

    Implement simple client for BA and KLM

--- lib/cheap_flights/integrations/air_france.ex
+++ lib/cheap_flights/integrations/air_france.ex
@@ -1,3 +1,26 @@
 defmodule CheapFlights.Integrations.AirFrance do
+  @moduledoc """
+  Client for Air France
+  """
+  import SweetXml
 
+  @behaviour CheapFlights.Behaviours.Integration
+  @provider "ba"
+  @endpoint "https://gist.githubusercontent.com/kanmaniselvan/bb11edf031e254977b210c480a0bd89a/raw/ea9bcb65ba4bb2304580d6202ece88aee38540f8/afklm_response_sample.xml"
+
+  @impl true
+  def load_data do
+    @endpoint
+    |> Tesla.get!()
+    |> Map.get(:body)
+    |> xpath(
+      ~x"//ns2:OffersGroup/ns2:CarrierOffers/ns2:Offer"l,
+      currency: ~x"./ns2:OfferItem/ns2:Price/ns2:TotalAmount/@CurCode",
+      price: ~x"./ns2:OfferItem/ns2:Price/ns2:TotalAmount/text()",
+      offer_id: ~x"./ns2:OfferID/text()",
+      segment_id: ~x"./ns2:OfferItem/ns2:FareDetail/ns2:FareComponent/ns2:PaxSegmentRefID/text()"l
+    )
+    |> Enum.map(&CheapFlights.Helpers.prepare_data(&1, @provider))
+    |> Task.await_many()
+  end
 end
--- lib/cheap_flights/integrations/british_airways.ex
+++ lib/cheap_flights/integrations/british_airways.ex
@@ -1,3 +1,26 @@
 defmodule CheapFlights.Integrations.BritishAirways do
+  @moduledoc """
+  Client for British Airways
+  """
+  import SweetXml
 
+  @behaviour CheapFlights.Behaviours.Integration
+  @provider "ba"
+  @endpoint "https://gist.githubusercontent.com/kanmaniselvan/bb11edf031e254977b210c480a0bd89a/raw/ea9bcb65ba4bb2304580d6202ece88aee38540f8/ba_response_sample.xml"
+
+  @impl true
+  def load_data do
+    @endpoint
+    |> Tesla.get!()
+    |> Map.get(:body)
+    |> xpath(
+      ~x"//AirlineOffers/AirlineOffer"l,
+      currency: ~x"./TotalPrice/SimpleCurrencyPrice/@Code",
+      price: ~x"./TotalPrice/SimpleCurrencyPrice/text()",
+      offer_id: ~x"./OfferID/text()",
+      segment_id: ~x"./PricedOffer/Associations/ApplicableFlight/FlightSegmentReference/@ref"l
+    )
+    |> Enum.map(&CheapFlights.Helpers.prepare_data(&1, @provider))
+    |> Task.await_many()
+  end
 end

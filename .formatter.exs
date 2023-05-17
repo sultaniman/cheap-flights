5af722c3f38b40d234e1ef3915dac2f92a05445c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-05-17 15:27:07 +0200

    Configure formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  import_deps: [:typed_struct],
+  export: [
+    locals_without_parens: [field: 2, field: 3]
+  ]
 ]

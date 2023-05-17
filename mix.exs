eca1e949873690c798fa66a85b75527bf1b967a5
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-05-17 21:46:57 +0200

    Add exvcr and hackney

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule CheapFlights.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        vcr: :test,
+        "vcr.delete": :test,
+        "vcr.check": :test,
+        "vcr.show": :test
+      ]
     ]
   end
 
@@ -22,11 +28,13 @@ defmodule CheapFlights.MixProject do
     [
       {:quantum, "~> 3.0"},
       {:tesla, "~> 1.7"},
+      {:hackney, "~> 1.18"},
       {:castore, "~> 1.0"},
       {:sweet_xml, "~> 0.7.3"},
       {:typed_struct, "~> 0.3.0"},
       {:plug_cowboy, "~> 2.6"},
       {:jason, "~> 1.4"},
+      {:exvcr, "~> 0.14.1", only: :test}
     ]
   end
 end

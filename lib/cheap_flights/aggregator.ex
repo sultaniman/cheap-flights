11e37ff2ed912b03b19e1e57427cb4e47d9f692a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-05-18 11:02:08 +0200

    Set state in genserver

--- lib/cheap_flights/aggregator.ex
+++ lib/cheap_flights/aggregator.ex
@@ -6,7 +6,7 @@ defmodule CheapFlights.Aggregator do
 
   # Client API
   def start_link(_) do
-    GenServer.start(@module, {}, name: @module)
+    GenServer.start_link(@module, {}, name: @module)
   end
 
   @spec update :: any()
@@ -20,21 +20,20 @@ defmodule CheapFlights.Aggregator do
     # Return empty tuple which later will be populated
     # with fetched and parsed data from flights data source
     # first item in the tuple will be offers
-    # the second is the list of flight segments to cross match
+    # second is the list of flight segments to cross match
+    # third is a mapping of segments by segment id
     {:ok, {}, {:continue, :load_data}}
   end
 
   @impl true
-  def handle_cast(:update, state) do
+  def handle_cast(:update, _state) do
     Logger.info("Updating flight data")
-    CheapFlights.Integrations.load_data()
-    {:noreply, state}
+    {:noreply, CheapFlights.Integrations.load_data()}
   end
 
   @impl true
   def handle_continue(:load_data, _state) do
     Logger.info("Loading initial flight data")
-    CheapFlights.Integrations.load_data()
-    {:noreply, {}}
+    {:noreply, CheapFlights.Integrations.load_data()}
   end
 end
